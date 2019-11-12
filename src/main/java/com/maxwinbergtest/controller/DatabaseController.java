95d9feacfd1bff792f291fe0f5bfdcf4972a6601
Ubuntu <applai@ApplaiWorkspace.0p1dnrmkxn0uhdq4uwknyibtoc.ax.internal.cloudapp.net>
2019-11-12 23:30:57 +0000

    Comment on controller

--- src/main/java/com/maxwinbergtest/controller/DatabaseController.java
+++ src/main/java/com/maxwinbergtest/controller/DatabaseController.java
@@ -2,6 +2,9 @@ package com.maxwinbergtest.controller;
 
 public class DatabaseController{
 
-
+/**
+ * I didnt use this class, but if i had more views here i would use a controller layer as a abstract layer to communicate with the model from the view
+ * This would add some logic in how the view could integrate with the model and have control over how the model components are used.
+ */
 
 }
